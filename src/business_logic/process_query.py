fee89e3880933713d9c6280db426c355c2188336
Antoniocsp88 <69919692+Antoniocsp88@users.noreply.github.com>
2020-10-12 23:09:50 -0400

    Update process_query.py

--- src/business_logic/process_query.py
+++ src/business_logic/process_query.py
@@ -20,7 +20,7 @@ class BusinessLogic:
         self._config = configparser.ConfigParser()
         self._config.read('application.conf')
         self._model_creator = model_creator
-        self._create_bucket()
+        #self._create_bucket()
 
     def get_version(self):
         return self._config['DEFAULT']['version']
@@ -29,22 +29,23 @@ class BusinessLogic:
         return f'{self._root_bucket}_{self.get_version().replace(".", "")}'
 
     def _get_or_create_model(self, ticker):
-        log = logging.getLogger()
-        model_filename = self.get_model_filename_from_ticker(ticker)
-        model = get_model_from_bucket(model_filename, self.get_bucket_name())
-        if model is None:
-            log.warning(f'training model for {ticker}')
-            model = self._model_creator.fit(ticker)
-            with open(model_filename, 'wb') as f:
-                joblib.dump(model, f)
-            upload_file_to_bucket(model_filename, self.get_bucket_name())
+        #log = logging.getLogger()
+        #model_filename = self.get_model_filename_from_ticker(ticker)
+        #model = get_model_from_bucket(model_filename, self.get_bucket_name())
+        #if model is None:
+            #log.warning(f'training model for {ticker}')
+            #model = self._model_creator.fit(ticker)
+            #with open(model_filename, 'wb') as f:
+                #joblib.dump(model, f)
+            #upload_file_to_bucket(model_filename, self.get_bucket_name())
+        model = self._model_creator.fit(ticker)
         return model
 
     def get_model_filename_from_ticker(self, ticker):
         return f'{ticker}.pkl'
 
-    def _create_bucket(self):
-        create_bucket(self.get_bucket_name())
+    #def _create_bucket(self):
+        #create_bucket(self.get_bucket_name())
 
     def do_predictions_for(self, ticker):
         model = self._get_or_create_model(ticker)
